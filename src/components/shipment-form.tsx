91c83947d3a41e69aa712d99767708a2a532b2e0
Moh Ali Mas'ud <alimasud603@gmail.com>
2025-08-18 05:40:20 +0000

    tapi jadi begini masalahnya

--- src/components/shipment-form.tsx
+++ src/components/shipment-form.tsx
@@ -307,11 +307,11 @@ export function ShipmentForm({ onSuccess, onCancel }: ShipmentFormProps) {
                         <TableRow>
                             <TableHead className="w-[80px]">Gambar</TableHead>
                             <TableHead>Nama Produk</TableHead>
-                            <TableHead className="min-w-[100px]">Jumlah</TableHead>
-                            <TableHead className="min-w-[150px]">Harga (Rp)</TableHead>
-                            <TableHead className="min-w-[150px]">Diskon (Rp)</TableHead>
-                            <TableHead className="min-w-[150px]">Pengemasan/pcs (Rp)</TableHead>
-                            <TableHead className="w-[160px] text-right">Subtotal</TableHead>
+                            <TableHead className="w-[100px]">Jumlah</TableHead>
+                            <TableHead className="w-[140px]">Harga (Rp)</TableHead>
+                            <TableHead className="w-[140px]">Diskon (Rp)</TableHead>
+                            <TableHead className="w-[170px]">Pengemasan/pcs (Rp)</TableHead>
+                            <TableHead className="w-[140px] text-right">Subtotal</TableHead>
                             <TableHead className="w-[50px]"></TableHead>
                         </TableRow>
                     </TableHeader>
