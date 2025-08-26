faddba609b0dbe6d9b4221fc87d02d19ae0b3451
Moh Ali Mas'ud <alimasud603@gmail.com>
2025-08-26 12:36:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/settings/products/page.tsx
+++ src/app/settings/products/page.tsx
@@ -52,6 +52,7 @@ import { format } from 'date-fns';
 import { id } from 'date-fns/locale';
 import { cn } from '@/lib/utils';
 import Papa from 'papaparse';
+import { Label } from '@/components/ui/label';
 
 
 const productFormSchema = z.object({
