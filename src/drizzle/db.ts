fffbef786ca99086f061d3c0ac144d6c96b3fa05
Moh Ali Mas'ud <alimasud603@gmail.com>
2025-09-03 06:59:42 +0000

    Suitable for long-lived, persistent connections Each client has a dedica

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -4,12 +4,6 @@ import * as schema from './schema';
 
 const connectionString = process.env.POSTGRES_URL!;
 
-// Opsi untuk menonaktifkan IPv6.
-// Ini diperlukan untuk mengatasi masalah konektivitas di beberapa lingkungan serverless seperti Vercel.
-const client = postgres(connectionString, {
-    host: new URL(connectionString).hostname, // Ekstrak host dari connection string
-    // @ts-ignore - Opsi 'family' mungkin tidak ada di type definition, tapi didukung oleh Node.js net module
-    family: 4, // Paksa penggunaan IPv4
-});
+const client = postgres(connectionString);
 
 export const db = drizzle(client, { schema, logger: true });
