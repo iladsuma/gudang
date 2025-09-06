ea65d4df13d65473ac98aa55d1223a5c897a5179
Moh Ali Mas'ud <alimasud603@gmail.com>
2025-09-06 09:38:19 +0000

    di dashboard itu bukan estimasi loh ya tapi nilai fix aja

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -286,7 +286,7 @@ export default function DashboardPage() {
             <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
                  <Card>
                     <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                        <CardTitle className="text-sm font-medium">Estimasi Saldo Kas</CardTitle>
+                        <CardTitle className="text-sm font-medium">Saldo Kas</CardTitle>
                         <PiggyBank className="h-4 w-4 text-muted-foreground" />
                     </CardHeader>
                     <CardContent>
